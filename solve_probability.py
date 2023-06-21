907a10201206a24885a8414c6a58264c94809d48
NolanBrb <nolanb99@gmail.com>
2023-06-21 17:20:45 +0200

    feat: added plot for different values of alpha

--- solve_probability.py
+++ solve_probability.py
@@ -9,17 +9,16 @@ from typing import Union
 
 plt.ion()
 
-R = 0.1  # Ratio between rotationnal diffusion and alignment strength
 N_points = 500
 d_theta = 2 * np.pi / N_points
 theta = np.linspace(-np.pi, np.pi, N_points)
 sintheta_minus = np.roll(np.sin(2 * theta), 1)
 sintheta_plus = np.roll(np.sin(2 * theta), -1)
 
-p_stat = np.exp(np.cos(2 * theta) / 2 / R) / (2 * np.pi * iv(0, 1 / 2 / R))
 
-
-def RHS(t: float, p: npt.NDArray[np.float_]) -> npt.NDArray[np.float_]:
+def RHS(
+    t: float, p: npt.NDArray[np.float_], R: float, y_t: npt.NDArray[np.float_]
+) -> npt.NDArray[np.float_]:
     """
     Given current time t and values of probabilities on the [0,2*pi] ring, returns the RHS of the system with spatial derivatives
     """
@@ -71,9 +70,9 @@ def gaussian(x, mu, sigma):
     return 1 / np.sqrt(2 * np.pi) / sigma * np.exp(-(((x % (np.pi) - mu) / sigma) ** 2))
 
 
-def mean_squared_diff(t, y):
-    tol = 2e-5
-    return np.mean((y - p_stat) ** 2) > tol
+def mean_squared_diff(t, y, R, y_t):
+    tol = 1e-6
+    return np.mean((y - y_t) ** 2) > tol
 
 
 mean_squared_diff.terminal = True
@@ -82,55 +81,62 @@ theta_0 = 0
 p_0 = gaussian(theta, theta_0, 0.1) + gaussian(np.pi - theta, theta_0, 0.1)
 p_0 /= p_0.sum() * d_theta
 
-sol = solve_ivp(RHS, t_span=(0, 20), y0=p_0, events=mean_squared_diff)
+R_list = [2.0, 0.1, 0.0008]
+alpha_list = [1 / 2 / R for R in R_list]
 
-plt.figure(0)
-plt.plot(theta, sol["y"].T[-1], label=f"Computed solution (t = {sol['t'][-1]:.3})")
-plt.plot(theta, p_stat, label="Stationnary solution (analytical)")
-plt.legend()
+fig, axes = plt.subplots(nrows=len(R_list), ncols=3, figsize=(19, 15))
 
-p = sol["y"].T[-1]
+for i, R in enumerate(R_list):
+    print(f"R={R}")
+    p_stat = np.exp(np.cos(2 * theta) / 2 / R) / (2 * np.pi * iv(0, 1 / 2 / R))
+    sol = solve_ivp(
+        RHS, t_span=(0, 10), y0=p_0, events=mean_squared_diff, args=(R, p_stat)
+    )
 
-n_arr = np.arange(-20, 21, 2)
-plt.figure(1)
-plt.scatter(n_arr, compute_fourier_coefficient(p_0, n_arr))
-plt.scatter(n_arr, compute_fourier_coefficient(p, n_arr))
+    ax1 = axes[i, 0]
+    ax1.plot(theta, sol["y"].T[-1], label=f"Computed solution (t = {sol['t'][-1]:.3})")
+    ax1.plot(theta, p_stat, label="Stationnary solution (analytical)")
+    ax1.legend(loc="upper center")
 
-# visualize_step = 1
-# plt.figure(2)
-# for t, y in zip(sol["t"][::visualize_step], sol["y"].T[::visualize_step]):
-#     plt.clf()
-#     plt.polar(theta, y)
-#     plt.title(f"t={t}")
-#     plt.pause(0.03)
+    ax2 = axes[i, 1]
 
-n_arr = np.arange(0, 21, 2)
-fourier_coeff = compute_fourier_coefficient(sol["y"], n_arr)
+    n_arr = np.arange(-20, 21, 2)
+    ax2.scatter(n_arr, compute_fourier_coefficient(p_0, n_arr), label="t=0")
+    ax2.scatter(
+        n_arr,
+        compute_fourier_coefficient(sol["y"][:, -1], n_arr),
+        label=f"t={sol['t'][-1]:.3}",
+    )
+    ax2.set_xticks(ticks=n_arr, labels=n_arr // 2)
+    ax2.legend()
 
-plt.figure(3)
-plt.plot(sol["t"], fourier_coeff, label=[rf"$p_{{{i//2}}}$" for i in n_arr])
-plt.legend(loc="lower right", ncol=3)
-plt.tight_layout()
+    ax3 = axes[i, 2]
+    n_arr = np.arange(0, 11, 2)
+    fourier_coeff = compute_fourier_coefficient(sol["y"], n_arr)
+    plot_until_time = 2
+    last_index = find_bounds_idx(plot_until_time, sol["t"])[1]
+    lines = ax3.plot(
+        sol["t"][1:last_index],
+        fourier_coeff[1:last_index, 1:],
+        label=[rf"$p_{{{i//2}}}$" for i in n_arr[1:]],
+    )
+    [
+        ax3.axhline(
+            2 / np.pi * iv(n // 2, 1 / 2 / R) / iv(0, 1 / 2 / R),
+            linestyle="-.",
+            color=lines[i].get_color(),
+        )
+        for (i, n) in enumerate(n_arr[1:])
+    ]
+    ax3.legend(ncol=3, prop={"size": 10})
 
+cols = [r"$P(\theta)$", r"Fourier coefficients", r"$p_n(t)$"]
+rows = [rf"$\alpha={alpha:.1E}$" for alpha in alpha_list]
 
-# t_arr = np.linspace(0, 3, 3)
-# for t in t_arr:
-#     p = sol_interpolated(sol, t)
+for ax, col in zip(axes[0], cols):
+    ax.set_title(col)
 
-#     n_arr = np.arange(-20, 21, 2)
-#     l_fourier = []
-#     for n in n_arr:
-#         l_fourier.append(compute_fourier_coefficient(p, n))
-#     plt.scatter(n_arr, l_fourier)
+for ax, row in zip(axes[:, 0], rows):
+    ax.set_ylabel(row, size="large")
 
-# ax = plt.gca()
-# _ = ax.set_ylim([-2 / np.pi - 0.1, 2 / np.pi + 0.1])
-# yticks = np.arange(-2 / np.pi, 2 / np.pi + 0.1, 1 / (np.pi))
-# n_ticks = len(yticks)
-# _ = ax.set_yticks(yticks)
-# labels = [
-#     rf"${n}\cdot$" + r"$\frac{1}{\pi}$"
-#     for n in range(-(n_ticks // 2), n_ticks // 2 + 1)
-# ]
-# _ = ax.set_yticklabels(labels)
-# ax.grid(linestyle="-.")
+fig.tight_layout()
