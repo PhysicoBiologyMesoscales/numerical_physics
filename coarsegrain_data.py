d33d0fa63743fd2cb02b2cd50812f6f6fec1d5e0
nolan <nolanb99@gmail.com>
2024-07-05 11:29:41 +0200

    Added linear regression model

--- coarsegrain_data.py
+++ coarsegrain_data.py
@@ -1,9 +1,9 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 import json
 import argparse
 import scipy.sparse as sp
+import xarray as xr
 
 
 from os.path import join
@@ -86,168 +86,61 @@ def main():
         )
 
         index = pd.MultiIndex.from_product(
-            [np.arange(Ny), np.arange(Nx), np.arange(Nth)], names=["y", "x", "theta"]
+            [np.arange(Ny) * dy, np.arange(Nx) * dx, np.arange(Nth) * dth],
+            names=["y", "x", "theta"],
         )
 
         cols = ["psi", "Fx", "Fy"]
 
         return pd.DataFrame(data, index=index, columns=cols)
 
-    def average_data(df):
-        rho = df["psi"].sum() * dth
-        rho_without_zero = np.where(rho == 0, 1.0, rho)
-        px = (df["psi"] * np.cos(df["theta"])).sum() * dth / rho_without_zero
-        py = (df["psi"] * np.sin(df["theta"])).sum() * dth / rho_without_zero
-        Fx = (df["psi"] * df["Fx"]).sum() * dth / rho_without_zero
-        Fy = (df["psi"] * df["Fy"]).sum() * dth / rho_without_zero
-        return pd.Series({"rho": rho, "px": px, "py": py, "Fx": Fx, "Fy": Fy})
+    def average_data(ds):
+        rho = ds["psi"].sum(dim="theta") * dth
+        rho_wo_zero = np.where(rho == 0, 1.0, rho)
+        px = (ds["psi"] * np.cos(ds["theta"])).sum(dim="theta") * dth / rho_wo_zero
+        py = (ds["psi"] * np.sin(ds["theta"])).sum(dim="theta") * dth / rho_wo_zero
+        Fx = (ds["psi"] * ds["Fx"]).sum(dim="theta") * dth / rho_wo_zero
+        Fy = (ds["psi"] * ds["Fy"]).sum(dim="theta") * dth / rho_wo_zero
+        return xr.Dataset(
+            data_vars=dict(
+                rho=(["t", "y", "x"], rho.data),
+                px=(["t", "y", "x"], px.data),
+                py=(["t", "y", "x"], py.data),
+                Fx=(["t", "y", "x"], Fx.data),
+                Fy=(["t", "y", "x"], Fy.data),
+            ),
+            coords=rho.coords,
+            attrs=ds.attrs,
+        )
 
-    cg_data = full_data.groupby("t").apply(coarsegrain_df)
-    avg_data = cg_data.reset_index(level=3).groupby(["t", "x", "y"]).apply(average_data)
+    cg_data = (
+        full_data.groupby("t")
+        .apply(coarsegrain_df)
+        .to_xarray()
+        .assign_attrs(
+            {
+                "l": l,
+                "L": L,
+                "Nx": Nx,
+                "Ny": Ny,
+                "Nth": Nth,
+                "dx": dx,
+                "dy": dy,
+                "dth": dth,
+            }
+        )
+    )
+    avg_data = average_data(cg_data)
 
-    cg_data.to_csv(join(sim_path, "cg_data.csv"))
-    avg_data.to_csv(join(sim_path, "avg_data.csv"))
-    with open(join(sim_path, "cg_parms.json"), "w") as jsonFile:
-        json.dump({"l": l, "L": L, "Nx": Nx, "Ny": Ny, "Nth": Nth}, jsonFile)
+    cg_data.to_netcdf(join(sim_path, "cg_data.nc"))
+    avg_data.to_netcdf(join(sim_path, "avg_data.nc"))
 
 
 if __name__ == "__main__":
     import sys
     from unittest.mock import patch
 
-    sim_path = r"C:\Users\nolan\Documents\PhD\Simulations\Data\Compute_forces\Batch\Gradient_oblique"
+    sim_path = r"C:\Users\nolan\Documents\PhD\Simulations\Data\Compute_forces\Batch\ar=1.5_N=40000_phi=1.0_v0=2.0_kc=3.0_k=4.5_h=0.0"
     args = ["prog", sim_path, "20", "20"]
     with patch.object(sys, "argv", args):
         main()
-
-
-#         rho = psi.sum(axis=2) * 2 * np.pi / Nth
-
-#         grad_rho_x = sobel(rho, axis=1, mode="wrap") / 8 / dx
-#         grad_rho_y = sobel(rho, axis=0, mode="wrap") / 8 / dy
-
-#         # # grad_rho_x = (np.roll(rho, -1, axis=0) - np.roll(rho, 1, axis=0)) / 2 / dx
-#         # # grad_rho_y = (np.roll(rho, 1, axis=1) - np.roll(rho, -1, axis=1)) / 2 / dy
-
-#         F_avg = (
-#             F_cg.sum(axis=2)
-#             * 2
-#             * np.pi
-#             / Nth
-#             / np.where(rho == 0, 1, rho)[..., np.newaxis]
-#         )
-#         Fx = F_avg[:, :, 0]
-#         Fy = F_avg[:, :, 1]
-#         p = (
-#             e_cg.sum(axis=2)
-#             * 2
-#             * np.pi
-#             / Nth
-#             / np.where(rho == 0, 1, rho)[..., np.newaxis]
-#         )
-#         px, py = p[:, :, 0], p[:, :, 1]
-#         data = np.stack(
-#             [
-#                 rho.reshape(Nx * Ny),
-#                 grad_rho_x.reshape(Nx * Ny),
-#                 grad_rho_y.reshape(Nx * Ny),
-#                 px.reshape(Nx * Ny),
-#                 py.reshape(Nx * Ny),
-#                 Fx.reshape(Nx * Ny),
-#                 Fy.reshape(Nx * Ny),
-#             ],
-#             axis=-1,
-#         )
-#         cols = ["rho", "grad_rho_x", "grad_rho_y", "px", "py", "Fx", "Fy"]
-#         index = np.arange(Nx * Ny)
-#         return pd.DataFrame(data, columns=cols, index=index)
-
-
-# data_folder = r"C:\Users\nolan\Documents\PhD\Simulations\Data\Compute_forces\Batch\Gradient_oblique"
-# df_full = pd.read_csv(join(data_folder, "Data.csv"), index_col=0)
-
-# with open(join(data_folder, "parms.json")) as json_file:
-#     parm_dic = json.load(json_file)
-
-# N = parm_dic["N"]
-# asp = parm_dic["aspect_ratio"]
-# l = np.sqrt(N * np.pi / asp / parm_dic["phi"])
-# L = asp * l
-
-# Nx, Nth = 20, 10
-# Ny = int(Nx * asp)
-# dx, dy = l / Nx, L / Ny
-
-
-# df_cg = df_full.groupby("t").apply(coarsegrain_df)
-
-# timepoints = df_cg.index.unique(level="t")
-
-# df_fit = df_cg
-# df_test = df_cg.loc[timepoints[10]]
-
-
-# def set_data(df):
-#     X = np.stack(
-#         [
-#             np.concatenate([df["grad_rho_x"], df["grad_rho_y"]]),
-#             np.concatenate([df["rho"] * df["px"], df["rho"] * df["py"]]),
-#         ],
-#         axis=-1,
-#     )
-#     y = np.concatenate([df["Fx"], df["Fy"]])
-#     return X, y
-
-
-# X_fit, y_fit = set_data(df_fit)
-# X_test, y_test = set_data(df_test)
-
-# reg = LinearRegression()
-# reg.fit(X_fit, y_fit)
-
-
-# def get_field(df, field):
-#     return np.array(df[field]).reshape((Ny, Nx))
-
-
-# rho = get_field(df_test, "rho")
-# Fx_true = get_field(df_test, "Fx")
-# Fy_true = get_field(df_test, "Fy")
-
-# y_pred = reg.predict(X_test)
-# F_pred = y_pred.reshape((Ny * Nx, 2), order="F").reshape((Ny, Nx, 2))
-
-# plt.imshow(rho, origin="lower")
-# plt.quiver(Fx_true, Fy_true)
-# plt.quiver(
-#     F_pred.reshape((Ny, Nx, 2))[:, :, 0],
-#     F_pred.reshape((Ny, Nx, 2))[:, :, 1],
-#     color=(1, 0, 0),
-# )
-
-# reg.score(X_test, y_test)
-
-
-# def test_regression(sim_folder_path, timepoint, reg):
-#     df = pd.read_csv(join(sim_folder_path, "Data.csv"), index_col=0)
-#     df_t = df[df["t"] == pd.unique(df["t"])[timepoint]]
-#     df_cg = coarsegrain_df(df_t)
-#     Xtest, ytest = set_data(df_cg)
-#     rho = get_field(df_cg, "rho")
-#     Fx_true = get_field(df_cg, "Fx")
-#     Fy_true = get_field(df_cg, "Fy")
-#     Fpred = reg.predict(Xtest)
-#     return rho, Fx_true, Fy_true, Fpred, Xtest, ytest
-
-
-# # grad_rho = np.stack([grad_rho_x, grad_rho_y], axis=-1)
-
-
-# # # X = grad_rho.reshape((Nx*Ny*2,1))
-# # y = F_avg.reshape(Nx * Ny * 2)
-# # reg = LinearRegression().fit(X, y)
-# # F_pred = reg.predict(X).reshape(Ny, Nx, 2)
-# # plt.figure(figsize=(10, asp * 10))
-# # plt.imshow(rho, origin="lower")
-# # plt.quiver(F_avg[:, :, 0], F_avg[:, :, 1])
-# # plt.quiver(F_pred[:, :, 0], F_pred[:, :, 1], color=(1, 0, 0))
--- view_coarsegraining.py
+++ view_coarsegraining.py
@@ -4,6 +4,8 @@ import holoviews as hv
 import panel as pn
 import json
 import argparse
+import xarray as xr
+from sklearn.linear_model import LinearRegression
 
 from os.path import join
 
@@ -18,62 +20,148 @@ def parse_args():
     return parser.parse_args()
 
 
+def set_data(ds):
+    X = np.stack(
+        [
+            np.concatenate(
+                [
+                    ds.grad_rho_x.data.flatten(),
+                    ds.grad_rho_y.data.flatten(),
+                ]
+            ),
+            np.concatenate(
+                [(ds.rho * ds.px).data.flatten(), (ds.rho * ds.py).data.flatten()]
+            ),
+        ],
+        axis=-1,
+    )
+    y = np.concatenate([ds.Fx.data.flatten(), ds.Fy.data.flatten()])
+    return X, y
+
+
 def main():
     parms = parse_args()
     sim_path = parms.sim_folder_path
-    with open(join(sim_path, "cg_parms.json")) as jsonFile:
-        parms = json.load(jsonFile)
+    # TODO Changer l'enregistrement des données pour charger directement des xarray
+    cg_ds = xr.open_dataset(join(sim_path, "cg_data.nc"))
+    avg_ds = xr.open_dataset(join(sim_path, "avg_data.nc"))
+    asp = cg_ds.attrs["L"] / cg_ds.attrs["l"]
 
-    asp = parms["L"] / parms["l"]
-    cg_data = pd.read_csv(join(sim_path, "cg_data.csv"))
-    avg_data = pd.read_csv(join(sim_path, "avg_data.csv"))
+    avg_ds = avg_ds.assign(
+        grad_rho_x=lambda arr: arr.rho.roll(x=-1) - arr.rho.roll(x=1)
+    )
+    avg_ds = avg_ds.assign(
+        grad_rho_y=lambda arr: arr.rho.roll(y=-1) - arr.rho.roll(y=1)
+    )
 
-    avg_data["F"] = np.sqrt(avg_data["Fx"] ** 2 + avg_data["Fy"] ** 2)
-    avg_data["F_angle"] = np.arctan2(avg_data["Fy"], avg_data["Fx"])
-    avg_data["p"] = np.sqrt(avg_data["px"] ** 2 + avg_data["py"] ** 2)
-    avg_data["p_angle"] = np.arctan2(avg_data["py"], avg_data["px"])
+    # Linear regression fit of the forces with fields (grad_rho, p)
+    X, y = set_data(avg_ds)
+    lr = LinearRegression()
+    lr.fit(X, y)
+    Nx, Ny = avg_ds.Nx, avg_ds.Ny
+    Fpred = lr.predict(X).reshape((2, avg_ds.sizes["t"], Ny, Nx))
+    avg_ds["Fpredx"] = (["t", "y", "x"], Fpred[0])
+    avg_ds["Fpredy"] = (["t", "y", "x"], Fpred[1])
+
+    # avg_ds.assign(F_pred = lambda x: lr.predict(x[]))
+
+    avg_ds["F"] = np.sqrt(avg_ds["Fx"] ** 2 + avg_ds["Fy"] ** 2)
+    avg_ds["F_angle"] = np.arctan2(avg_ds["Fy"], avg_ds["Fx"])
+    avg_ds["p"] = avg_ds["rho"] * np.sqrt(avg_ds["px"] ** 2 + avg_ds["py"] ** 2)
+    avg_ds["p_angle"] = np.arctan2(avg_ds["py"], avg_ds["px"])
+    avg_ds["Fpred"] = np.sqrt(avg_ds["Fpredx"] ** 2 + avg_ds["Fpredy"] ** 2)
+    avg_ds["Fpred_angle"] = np.arctan2(avg_ds["Fpredy"], avg_ds["Fpredx"])
 
     plot_F = pn.widgets.Checkbox(name="F")
+    color_F = pn.widgets.ColorPicker(name="F color", value="red")
+    plot_Fpred = pn.widgets.Checkbox(name="Predicted")
+    color_Fpred = pn.widgets.ColorPicker(name="Predicted F color", value="green")
     plot_p = pn.widgets.Checkbox(name="p")
+    color_p = pn.widgets.ColorPicker(name="p color", value="yellow")
     select_cmap = pn.widgets.Select(
-        name="Color Map", value="blues", options=["blues", "jet"]
+        name="Color Map", value="blues", options=["blues", "jet", "Reds"]
     )
-    list_t = list(pd.unique(avg_data["t"]))
+    list_t = list(avg_ds.t.data)
     t_slider = pn.widgets.DiscreteSlider(name="t", options=list_t)
 
-    def plot_data(t, cmap, plot_F, plot_p):
-        t_data = avg_data[avg_data["t"] == t]
+    def plot_data(t, cmap, plot_F, plot_p, plot_Fpred, f_col, p_col, fpred_col):
+        t_data = avg_ds.sel(t=t)
         alpha_F = 0
         alpha_p = 0
+        alpha_Fpred = 0
         if plot_F:
             alpha_F = 1
         if plot_p:
             alpha_p = 1
+        if plot_Fpred:
+            alpha_Fpred = 1
         return (
-            hv.HeatMap((t_data["x"], t_data["y"], t_data["rho"])).opts(cmap=cmap)
+            hv.HeatMap((t_data["x"], t_data["y"], t_data["rho"])).opts(
+                cmap=cmap,
+                clim=(float(avg_ds.rho.min()), float(avg_ds.rho.max())),
+                width=400,
+                height=int(asp * 400),
+            )
             * hv.VectorField(
-                (t_data["x"], t_data["y"], t_data["F_angle"], t_data["F"])
-            ).opts(alpha=alpha_F, color=(1, 0, 0))
+                (
+                    t_data["x"],
+                    t_data["y"],
+                    t_data["F_angle"],
+                    t_data["F"] / t_data["F"].mean(),
+                )
+            )
+            .opts(alpha=alpha_F, color=f_col)
+            .opts(magnitude=hv.dim("Magnitude").norm())
+            * hv.VectorField((t_data["x"], t_data["y"], t_data["p_angle"], t_data["p"]))
+            .opts(alpha=alpha_p, color=p_col)
+            .opts(magnitude=hv.dim("Magnitude").norm())
             * hv.VectorField(
-                (t_data["x"], t_data["y"], t_data["p_angle"], t_data["p"])
-            ).opts(alpha=alpha_p)
+                (
+                    t_data["x"],
+                    t_data["y"],
+                    t_data["Fpred_angle"],
+                    t_data["Fpred"] / t_data["F"].mean(),
+                )
+            )
+            .opts(alpha=alpha_Fpred, color=fpred_col)
+            .opts(magnitude=hv.dim("Magnitude").norm())
         )
 
     dmap = hv.DynamicMap(
-        pn.bind(plot_data, t=t_slider, cmap=select_cmap, plot_F=plot_F, plot_p=plot_p)
+        pn.bind(
+            plot_data,
+            t=t_slider,
+            cmap=select_cmap,
+            plot_F=plot_F,
+            f_col=color_F,
+            plot_p=plot_p,
+            p_col=color_p,
+            plot_Fpred=plot_Fpred,
+            fpred_col=color_Fpred,
+        )
     )
 
-    row = pn.Row(pn.Column(t_slider, select_cmap, plot_F, plot_p), dmap)
+    row = pn.Row(
+        pn.Column(
+            t_slider,
+            select_cmap,
+            pn.Row(plot_F, color_F),
+            pn.Row(plot_p, color_p),
+            pn.Row(plot_Fpred, color_Fpred),
+        ),
+        dmap,
+    )
 
-    return cg_data, avg_data, plot, row
+    return cg_ds, avg_ds, row, lr
 
 
 if __name__ == "__main__":
     import sys
     from unittest.mock import patch
 
+    pn.extension()
     sim_path = r"C:\Users\nolan\Documents\PhD\Simulations\Data\Compute_forces\Batch\Gradient_oblique"
     args = ["prog", sim_path]
     with patch.object(sys, "argv", args):
-        cg_data, avg_data, plot, row = main()
+        cg_ds, avg_ds, row, lr = main()
         row
