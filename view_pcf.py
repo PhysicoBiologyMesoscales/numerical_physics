6f139bc9ae69c25867d09a678ee2e0d1f51b328e
nolan <nolanb99@gmail.com>
2024-07-18 16:54:16 +0200

    Aded averaging options in pcf visualizer

--- view_pcf.py
+++ view_pcf.py
@@ -22,25 +22,36 @@ def main():
     sim_path = parms.sim_folder_path
     pcf_ds = xr.open_dataset(join(sim_path, "pcf.nc"))
 
-    asp = pcf_ds.asp
+    # Divide by proba to find particle with given orientation
+    pcf_ds = pcf_ds.assign(g2=pcf_ds.g / pcf_ds.g.sum(dim=["r", "phi"]))
 
+    t_avg_checkbox = pn.widgets.Checkbox(name="Time Average")
+    th_avg_checkbox = pn.widgets.Checkbox(name="Theta Average")
     list_t = list(pcf_ds.t.data)
     t_slider = pn.widgets.DiscreteSlider(name="t", options=list_t)
     list_th = list(pcf_ds.theta.data)
     th_slider = pn.widgets.DiscreteSlider(name="theta", options=list_th)
     select_cmap = pn.widgets.Select(
-        name="Color Map", value="viridis", options=["viridis", "jet", "blues", "Reds"]
+        name="Color Map", value="viridis", options=["viridis", "jet", "bwr"]
     )
 
     # TODO Fix xticks !
-    def plot_data(t, th, cmap):
-        data = pcf_ds.sel(t=t, theta=th)
-        plot = hv.HeatMap((data["phi"], data["r"], data["g"])).opts(
+    def plot_data(t_avg, th_avg, t, th, cmap):
+        _sel = {"t": t, "theta": th}
+        avg_dims = []
+        if t_avg:
+            avg_dims.append("t")
+            _sel.pop("t")
+        if th_avg:
+            avg_dims.append("theta")
+            _sel.pop("theta")
+        data = pcf_ds.mean(dim=avg_dims).sel(**_sel)
+        plot = hv.HeatMap((data["phi"], data["r"], data["g2"])).opts(
             cmap=cmap,
             width=400,
             height=400,
-            yticks=list(np.arange(6)),
-            clim=(float(pcf_ds.g.min()), float(pcf_ds.g.max())),
+            yticks=list(np.round(np.linspace(0, float(pcf_ds.r.max()), 5), decimals=1)),
+            clim=(float(pcf_ds.g2.min()), float(pcf_ds.g2.max())),
             radial=True,
             radius_inner=0,
             tools=["hover"],
@@ -50,15 +61,28 @@ def main():
     dmap = hv.DynamicMap(
         pn.bind(
             plot_data,
+            t_avg=t_avg_checkbox,
+            th_avg=th_avg_checkbox,
             t=t_slider,
             th=th_slider,
             cmap=select_cmap,
         )
     )
 
+    def disable_t(x, y):
+        t_slider.disabled = x
+
+    def disable_th(x, y):
+        th_slider.disabled = x
+
+    pn.bind(disable_t, t_avg_checkbox, t_slider, watch=True)
+    pn.bind(disable_th, th_avg_checkbox, th_slider, watch=True)
+
     row = pn.Row(
         pn.WidgetBox(
             pn.Column(
+                t_avg_checkbox,
+                th_avg_checkbox,
                 t_slider,
                 th_slider,
                 select_cmap,
@@ -74,9 +98,7 @@ if __name__ == "__main__":
     from unittest.mock import patch
 
     pn.extension()
-    sim_path = (
-        r"C:\Users\nolan\Documents\PhD\Simulations\Data\Compute_forces\Batch\test"
-    )
+    sim_path = r"C:\Users\nolan\Documents\PhD\Simulations\Data\Compute_forces\Batch\ar=1.5_N=40000_phi=1.0_v0=3.0_kc=3.0_k=10.0_h=0.0_tmax=1.0"
     args = ["prog", sim_path]
     with patch.object(sys, "argv", args):
         pcf_ds, row = main()
