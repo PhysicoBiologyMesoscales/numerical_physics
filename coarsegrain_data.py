Corrected average forces computation

coarsegrain_data.py
@@ -139,12 +139,12 @@ def main():
         # TODO check : je pense que c'est déjà psi*F que je moyenne
         Fx_avg=(
             ["t", "y", "x"],
-            (cg_data.psi * cg_data.Fx).sum(dim="theta").data * dth / rho_wo_zero,
+            (cg_data.Fx).sum(dim="theta").data * dth / rho_wo_zero,
             {"name": "F_avg", "average": 1, "type": "vector", "dir": "x"},
         ),
         Fy_avg=(
             ["t", "y", "x"],
-            (cg_data.psi * cg_data.Fy).sum(dim="theta").data * dth / rho_wo_zero,
+            (cg_data.Fy).sum(dim="theta").data * dth / rho_wo_zero,
             {"name": "F_avg", "average": 1, "type": "vector", "dir": "y"},
         ),
     )