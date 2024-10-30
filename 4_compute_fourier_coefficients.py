89d23d18a9722120d2cf9b70ffc09ce6fe99c9a5
nolan <nolanb99@gmail.com>
2024-10-30 18:30:30 +0100

    feat: fourier coefficients computation now stored in hdf format; removed hydrodynamic parameter computation

--- 4_compute_fourier_coefficients.py
+++ 4_compute_fourier_coefficients.py
@@ -1,9 +1,9 @@
 import numpy as np
-import xarray as xr
 import argparse
-import json
+import h5py
 
 from os.path import join
+from itertools import product
 
 
 def parse_args():
@@ -20,115 +20,134 @@ def main():
     args = parse_args()
     sim_path = args.sim_folder_path
 
-    # Load data; select only for r<2 (particle_particle interaction range)
-    pcf_ds = xr.open_dataset(join(sim_path, "pcf.nc")).sel(r=slice(0, 2))
-    ds = pcf_ds.sel(t=slice(pcf_ds.dt_save, None)).mean(dim="t")
+    # Load data; select only t>0
+    with h5py.File(join(sim_path, "data.h5py"), "a") as hdf_file:
+        pcf_grp = hdf_file["pair_correlation"]
+        # Recover pcf coordinates
+        r = pcf_grp["r"][()].flatten()
+        phi = pcf_grp["phi"][()]
+        theta = pcf_grp["theta"][()]
+        delta_theta = pcf_grp["d_theta"][()]
 
-    kc = pcf_ds.kc
-    v0 = pcf_ds.v0
-    dr2 = pcf_ds.dr2 / 2
-    dphi = pcf_ds.dphi
-    dth = pcf_ds.dth
+        pcf_full = pcf_grp["pcf"][1:]
+        # Average over time
+        pcf = pcf_full.mean(axis=0)
 
-    def compute_fourier_coeff(ds, l: int, m: int):
-        """
-        Compute the 4 Fourier coefficients associated with integers (l,m).
-        Coefficients can be 0 if any of l,m equals 0.
-        """
-        mul = 4 / (2 * np.pi) ** 2
-        if l == 0:
-            mul /= 2
-        if m == 0:
-            mul /= 2
-        a_lm = (
-            mul * ds.g * np.cos(l * ds.phi) * np.cos(m * ds.theta) * dth * dphi
-        ).sum(dim=["theta", "phi"])
-        b_lm = (
-            mul * ds.g * np.sin(l * ds.phi) * np.cos(m * ds.theta) * dth * dphi
-        ).sum(dim=["theta", "phi"])
-        c_lm = (
-            mul * ds.g * np.cos(l * ds.phi) * np.sin(m * ds.theta) * dth * dphi
-        ).sum(dim=["theta", "phi"])
-        d_lm = (
-            mul * ds.g * np.sin(l * ds.phi) * np.sin(m * ds.theta) * dth * dphi
-        ).sum(dim=["theta", "phi"])
-        return (a_lm, b_lm, c_lm, d_lm)
+        dr2 = pcf_grp.attrs["dr2"] / 2
+        dphi = pcf_grp.attrs["dphi"]
+        dth = pcf_grp.attrs["dth"]
+        ddth = pcf_grp.attrs["ddth"]
 
-    # Compute force array
-    def F(r):
-        return kc * v0 * (2 - np.where(r < 2, r, 0))
+        def compute_fourier_coeff(pcf, l: int, m: int, n: int):
+            """
+            Compute the 8 Fourier coefficients associated with integers (l,m,n).
+            Coefficients can be 0 if any of l,m equals 0.
+            Arguments:
+                pcf - array containing pair-correlation function; dimensions are (r, phi, theta, delta_theta)
+                l, m, n - integers used to characterize Fourier mode
+            """
+            mul = 8 / (2 * np.pi) ** 3
+            if l == 0:
+                mul /= 2
+            if m == 0:
+                mul /= 2
+            if n == 0:
+                mul /= 2
+            a_lmn = (
+                mul
+                * pcf
+                * np.cos(l * phi)
+                * np.cos(m * delta_theta)
+                * np.cos(n * theta)
+                * dth
+                * dphi
+                * ddth
+            ).sum(axis=(1, 2, 3))
+            b_lmn = (
+                mul
+                * pcf
+                * np.sin(l * phi)
+                * np.cos(m * delta_theta)
+                * np.cos(n * theta)
+                * dth
+                * dphi
+                * ddth
+            ).sum(axis=(1, 2, 3))
+            c_lmn = (
+                mul
+                * pcf
+                * np.cos(l * phi)
+                * np.sin(m * delta_theta)
+                * np.cos(n * theta)
+                * dth
+                * dphi
+                * ddth
+            ).sum(axis=(1, 2, 3))
+            d_lmn = (
+                mul
+                * pcf
+                * np.sin(l * phi)
+                * np.sin(m * delta_theta)
+                * np.cos(n * theta)
+                * dth
+                * dphi
+                * ddth
+            ).sum(axis=(1, 2, 3))
+            e_lmn = (
+                mul
+                * pcf
+                * np.cos(l * phi)
+                * np.cos(m * delta_theta)
+                * np.sin(n * theta)
+                * dth
+                * dphi
+                * ddth
+            ).sum(axis=(1, 2, 3))
+            f_lmn = (
+                mul
+                * pcf
+                * np.sin(l * phi)
+                * np.cos(m * delta_theta)
+                * np.sin(n * theta)
+                * dth
+                * dphi
+                * ddth
+            ).sum(axis=(1, 2, 3))
+            g_lmn = (
+                mul
+                * pcf
+                * np.cos(l * phi)
+                * np.sin(m * delta_theta)
+                * np.sin(n * theta)
+                * dth
+                * dphi
+                * ddth
+            ).sum(axis=(1, 2, 3))
+            h_lmn = (
+                mul
+                * pcf
+                * np.sin(l * phi)
+                * np.sin(m * delta_theta)
+                * np.sin(n * theta)
+                * dth
+                * dphi
+                * ddth
+            ).sum(axis=(1, 2, 3))
 
-    F_arr = F(ds.r)
+            return a_lmn, b_lmn, c_lmn, d_lmn, e_lmn, f_lmn, g_lmn, h_lmn
 
-    # Dictionnary which will hold all hydrodynamic parameters
-    parms = {}
+        if "fourier_coefficients" in hdf_file:
+            del hdf_file["fourier_coefficients"]
 
-    for m in range(3):
-        parms[f"a_1{m}"] = -float(
-            (np.pi * ds.r * compute_fourier_coeff(ds, 1, m)[0] * F_arr * dr2).sum(
-                dim="r"
-            )
-        )
-        parms[f"a_2{m}"] = -float(
-            (np.pi * ds.r * compute_fourier_coeff(ds, 1, m)[3] * F_arr * dr2).sum(
-                dim="r"
-            )
-        )
-        parms[f"a_3{m}"] = -float(
-            (np.pi * ds.r**2 * compute_fourier_coeff(ds, 0, m)[0] * F_arr * dr2).sum(
-                dim="r"
-            )
-        )
-        parms[f"a_4{m}"] = -float(
-            (np.pi * ds.r**2 * compute_fourier_coeff(ds, 2, m)[0] * F_arr * dr2).sum(
-                dim="r"
-            )
-        )
-        parms[f"a_5{m}"] = -float(
-            (np.pi * ds.r**2 * compute_fourier_coeff(ds, 2, m)[3] * F_arr * dr2).sum(
-                dim="r"
-            )
-        )
-        parms[f"a_6{m}"] = -float(
-            (
-                np.pi / 8 * ds.r**3 * compute_fourier_coeff(ds, 1, m)[0] * F_arr * dr2
-            ).sum(dim="r")
-        )
-        parms[f"a_7{m}"] = -float(
-            (
-                np.pi / 2 * ds.r**3 * compute_fourier_coeff(ds, 3, m)[0] * F_arr * dr2
-            ).sum(dim="r")
-        )
-        parms[f"a_8{m}"] = -float(
-            (
-                np.pi / 8 * ds.r**3 * compute_fourier_coeff(ds, 1, m)[3] * F_arr * dr2
-            ).sum(dim="r")
-        )
-        parms[f"a_9{m}"] = -float(
-            (
-                np.pi / 2 * ds.r**3 * compute_fourier_coeff(ds, 3, m)[3] * F_arr * dr2
-            ).sum(dim="r")
-        )
-        parms[f"c_1{m}"] = parms[f"a_2{m}"]
-        parms[f"c_2{m}"] = -(parms[f"a_3{m}"] + 0.5 * parms[f"a_4{m}"])
-        parms[f"c_3{m}"] = parms[f"a_5{m}"] / 2
-        parms[f"c_4{m}"] = -2 * (parms[f"a_6{m}"] - 0.25 * parms[f"a_7{m}"])
-        parms[f"c_5{m}"] = -2 * (parms[f"a_8{m}"] - 0.25 * parms[f"a_9{m}"])
-        parms[f"c_6{m}"] = 2 * parms[f"a_8{m}"]
-
-    with open(join(sim_path, "hydro_parms.json"), "w") as jsonFile:
-        json.dump(parms, jsonFile)
+        coeff_grp = hdf_file.create_group("fourier_coefficients")
+        coeff_grp.attrs["dr2"] = dr2
+        for l, m, n in product(range(3), range(3), range(3)):
+            grp = coeff_grp.create_group(f"{l}{m}{n}")
+            for coeff_type, coeff in zip(
+                list("abcdefgh"), compute_fourier_coeff(pcf, l, m, n)
+            ):
+                grp.create_dataset(f"{coeff_type}_{l}{m}{n}", data=coeff)
 
 
 if __name__ == "__main__":
-    import sys
-    from unittest.mock import patch
-
-    with open("save_parms.json") as jsonFile:
-        save_parms = json.load(jsonFile)
-        sim_path = join(save_parms["base_folder"], save_parms["sim"])
-
-    args = ["prog", sim_path]
-
-    with patch.object(sys, "argv", args):
-        main()
+    main()
