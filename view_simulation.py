a9c7cf2ad6239801c2723f7a3fbf8d0890972288
NolanBrb <nolanb99@gmail.com>
2024-10-31 18:24:18 +0100

    feat: added support to save gif of simulation and run visualisation using panel serve

--- view_simulation.py
+++ view_simulation.py
@@ -17,98 +17,101 @@ def parse_args():
     return parser.parse_args()
 
 
+parms = parse_args()
+
+## Load data
+sim_path = parms.sim_folder_path
+hdf_file = h5py.File(join(sim_path, "data.h5py"))
+N = hdf_file.attrs["N"]
+asp = hdf_file.attrs["asp"]
+sim_data = hdf_file["simulation_data"]
+
+## Create widgets
+list_t = list(sim_data["t"][()].flatten())
+t_slider = pn.widgets.DiscreteSlider(name="t", options=list_t)
+select_visualizer = pn.widgets.Select(
+    name="Visualizer",
+    value="theta",
+    options=["random", "theta", "Fx", "Fy"],
+)
+select_cmap = pn.widgets.Select(
+    name="Color Map", value="bwr", options=["viridis", "jet", "blues", "bwr"]
+)
+
+# Random color for visualization
+rand_color = np.arange(N)
+
+
+def plot_data(t, vis_field, cmap):
+    t_idx = list_t.index(t)
+
+    coords = np.array(
+        [
+            sim_data["r"][t_idx].real,
+            sim_data["r"][t_idx].imag,
+            rand_color,
+            sim_data["theta"][t_idx],
+            np.cos(sim_data["theta"][t_idx]),
+            np.sin(sim_data["theta"][t_idx]),
+            sim_data["F"][t_idx].real,
+            sim_data["F"][t_idx].imag,
+        ]
+    ).T
+
+    kwargs = {}
+    match vis_field:
+        case "random":
+            kwargs["clim"] = (0, N)
+        case "theta":
+            kwargs["clim"] = (0, np.pi)
+        case "px" | "py":
+            kwargs["clim"] = (-1.0, 1.0)
+        case "Fx":
+            Fmax = float(abs(sim_data["F"][()].real).max())
+            kwargs["clim"] = (-Fmax, Fmax)
+        case "Fy":
+            Fmax = float(abs(sim_data["F"][()].imag).max())
+            kwargs["clim"] = (-Fmax, Fmax)
+
+    plot = hv.Points(coords, vdims=["random", "theta", "px", "py", "Fx", "Fy"]).opts(
+        width=800,
+        height=int(asp * 800),
+        cmap=cmap,
+        size=1,
+        color=vis_field,
+        **kwargs,
+    )
+    return plot
+
+
+hv.extension("bokeh")
+pn.extension()
+dmap = hv.DynamicMap(
+    pn.bind(
+        plot_data,
+        t=t_slider,
+        vis_field=select_visualizer,
+        cmap=select_cmap,
+    )
+)
+
+row = pn.Row(
+    pn.WidgetBox(
+        pn.Column(
+            t_slider,
+            select_visualizer,
+            select_cmap,
+        )
+    ),
+    dmap,
+)
+row.servable()
+
+
 def main():
-    parms = parse_args()
-    sim_path = parms.sim_folder_path
-    hdf_file = h5py.File(join(sim_path, "data.h5py"))
-    N = hdf_file.attrs["N"]
-    asp = hdf_file.attrs["asp"]
-    v0 = hdf_file.attrs["v0"]
-
-    sim_data = hdf_file["simulation_data"]
-
-    list_t = list(sim_data["t"][()].flatten())
-    t_slider = pn.widgets.DiscreteSlider(name="t", options=list_t)
-    select_visualizer = pn.widgets.Select(
-        name="Visualizer",
-        value="theta",
-        options=["random", "vx", "vy", "theta", "Fx", "Fy"],
-    )
-    select_cmap = pn.widgets.Select(
-        name="Color Map", value="bwr", options=["viridis", "jet", "blues", "bwr"]
-    )
-
-    rand_color = np.arange(N)
-
-    def plot_data(t, vis_field, cmap):
-        t_idx = list_t.index(t)
-
-        coords = np.array(
-            [
-                sim_data["r"][t_idx].real,
-                sim_data["r"][t_idx].imag,
-                rand_color,
-                sim_data["theta"][t_idx],
-                # sim_data["v"][t_idx].real,
-                # sim_data["v"][t_idx].imag,
-                sim_data["F"][t_idx].real,
-                sim_data["F"][t_idx].imag,
-            ]
-        ).T
-
-        kwargs = {}
-        match vis_field:
-            case "random":
-                kwargs["clim"] = (0, N)
-            case "theta":
-                kwargs["clim"] = (0, 2 * np.pi)
-            # case "vx" | "vy":
-            #     kwargs["clim"] = (-v0, v0)
-            case "Fx":
-                Fmax = float(abs(sim_data["Fx"]).max())
-                kwargs["clim"] = (-Fmax, Fmax)
-            case "Fy":
-                Fmax = float(abs(sim_data["Fy"]).max())
-                kwargs["clim"] = (-Fmax, Fmax)
-
-        plot = hv.Points(coords, vdims=["random", "theta", "Fx", "Fy"]).opts(
-            width=400,
-            height=int(asp * 400),
-            cmap=cmap,
-            size=0.1,
-            color=vis_field,
-            **kwargs,
-        )
-        return plot
-
-    dmap = hv.DynamicMap(
-        pn.bind(
-            plot_data,
-            t=t_slider,
-            vis_field=select_visualizer,
-            cmap=select_cmap,
-        )
-    )
-
-    row = pn.Row(
-        pn.WidgetBox(
-            pn.Column(
-                t_slider,
-                select_visualizer,
-                select_cmap,
-            )
-        ),
-        dmap,
-    )
-    return hdf_file, row
+    anim = hv.HoloMap({t: plot_data(t, "px", "bwr") for t in list_t})
+    hv.save(anim, join(sim_path, "sim.gif"), backend="bokeh", fps=12)
 
 
 if __name__ == "__main__":
-    import sys
-    from unittest.mock import patch
-
-    pn.extension()
-    sim_path = r"C:\Users\nolan\Documents\PhD\Simulations\Data\Compute_forces\Batch\Test_rebuild"
-    args = ["prog", sim_path]
-    with patch.object(sys, "argv", args):
-        hdf_file, row = main()
+    main()
