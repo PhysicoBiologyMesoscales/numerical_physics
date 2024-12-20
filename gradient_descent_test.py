38bb00449e1bb27d94cb2e8b2bb950ac9d119edf
NolanBrb <nolanb99@gmail.com>
2024-12-20 18:51:50 +0100

    feat: added gradient descent calculation code

--- gradient_descent_test.py
+++ gradient_descent_test.py
@@ -1,163 +1,176 @@
 import numpy as np
 import h5py
 import matplotlib.pyplot as plt
+import matplotlib
 from os.path import join
 from scipy.special import iv
-from scipy.integrate import trapezoid
+from scipy.integrate import trapezoid, cumulative_trapezoid
 from scipy.spatial import KDTree
-from scipy.stats import binned_statistic_dd
+from scipy.stats import binned_statistic_dd, norm
 from itertools import product
 
 
-sim_path = r"C:\Users\nolan\Documents\PhD\Simulations\Test_gradient_descent"
+sim_path = r"E:\Local_Sims\test_new_code"
 
-Nth = 100
-theta_bins = np.linspace(0, 2 * np.pi, Nth + 1)
-theta = (theta_bins[:-1] + theta_bins[1:]) / 2
-
-n_modes = (10, 5)
+matplotlib.use("TkAgg")
 
 with h5py.File(join(sim_path, "data.h5py")) as hdf_file:
-    r = hdf_file["simulation_data"]["r"][-1]
-    theta_sim = hdf_file["simulation_data"]["theta"][-1]
-    pcf = hdf_file["pair_correlation"]["pcf"][()]
-    l = hdf_file.attrs["l"]
-    L = hdf_file.attrs["L"]
-    N = hdf_file.attrs["N"]
     kc = hdf_file.attrs["kc"]
-    # compute fourier coeffs integrals
-    fourier = hdf_file["fourier_coefficients"]
-    radial_coord = hdf_file["pair_correlation"]["r"][()].flatten()
-    d_array = np.zeros(n_modes + (len(radial_coord),))
-    f_array = np.zeros(n_modes + (len(radial_coord),))
-    for (i, m), (j, n) in product(
-        enumerate(range(n_modes[0])), enumerate(range(n_modes[1]))
-    ):
-        d_array[i, j, :] = fourier[f"1{m}{n}"][:, 3]
-        f_array[i, j, :] = fourier[f"1{m}{n}"][:, 5]
+    rho = hdf_file.attrs["N"] / hdf_file.attrs["l"] / hdf_file.attrs["L"]
+    corr = hdf_file["pair_correlation"]
+    Nth = corr.attrs["Nth"]
+    r = corr["r"][()]
+    phi = corr["phi"][()]
+    theta = corr["theta"][()]
+    dth = corr.attrs["dth"]
+    dphi = corr.attrs["dphi"]
+    pcf = corr["pcf"][()]
+    p_th = corr["p_th"][1:].mean(axis=0)
 
-max_r_idx = np.nonzero(radial_coord < 2)[0][-1]
-rad = radial_coord[: max_r_idx + 1]
-alpha = np.pi * trapezoid(
-    rad * kc * (2 - rad) * d_array[:, :, : max_r_idx + 1], rad, axis=-1
-)
-beta = np.pi * trapezoid(
-    rad * kc * (2 - rad) * f_array[:, :, : max_r_idx + 1], rad, axis=-1
-)
-coeffs = np.zeros(n_modes + (2,))
-coeffs[:, :, 0] = (alpha + beta) / (
-    2 * np.arange(0, n_modes[1])[None, :] + np.arange(0, n_modes[0])[:, None]
-)
-coeffs[:, :, 1] = (alpha - beta) / (
-    2 * np.arange(0, n_modes[1])[None, :] - np.arange(0, n_modes[0])[:, None]
-)
-coeffs = np.nan_to_num(coeffs, posinf=0, neginf=0)
+i_indices = np.arange(Nth).reshape(-1, 1)  # Column vector for row indices
+j_indices = np.arange(Nth)
+pcf_th = pcf[:, :, i_indices, (j_indices - i_indices) % Nth]
 
-cos = np.array(
-    [
-        [
-            [np.cos((2 * n + m) * theta), np.cos((2 * n - m) * theta)]
-            for n in range(n_modes[1])
-        ]
-        for m in range(n_modes[0])
-    ]
+F = -kc * (2 - r)
+
+integrand = (
+    np.einsum(
+        "ijkl,i,j,k,l->ijkl", pcf_th, -r * F, np.exp(1j * phi), np.exp(1j * theta), p_th
+    )
+    * rho
+)
+F_stat = trapezoid(
+    trapezoid(trapezoid(integrand, theta, axis=-1), phi, axis=1), r, axis=0
 )
 
-r = np.stack([r.real, r.imag], axis=-1)
+C = -(F_stat * np.exp(-1j * theta)).imag
 
-Nx = int(l / 10)
-Ny = int(L / 10)
-x_bins = np.linspace(0, l, Nx + 1)
-y_bins = np.linspace(0, L, Ny + 1)
-dx, dy, dth = l / Nx, L / Ny, 2 * np.pi / Nth
-
-data = np.stack([r[:, 0], r[:, 1], theta_sim], axis=-1)
-psi_0 = binned_statistic_dd(
-    data, 0, bins=(x_bins, y_bins, theta_bins), statistic="count"
-).statistic
-psi_0 /= N * dx * dy * dth
-
-px_avg = (np.cos(theta[None, :]) * psi_0).sum(axis=(0, 2)) * dx * dth
-psi_0 = np.where((px_avg < 0)[None, :, None], np.roll(psi_0, Nth // 2, axis=2), psi_0)
-
-psi_star = psi_0.sum(axis=(0, 1)) * dx * dy
-psi_star /= trapezoid(psi_star, theta)
-
-
-# number of (m,n) modes
-p_star = np.array(
-    [(np.cos(k * theta) * psi_star).sum() * dth for k in range(n_modes[0])]
-)
+# f = (
+#     cumulative_trapezoid(C, theta, initial=0)
+#     + cumulative_trapezoid(C[::-1], theta[::-1], initial=0)[::-1]
+# ) / 2
+f = (np.cumsum(C) - np.cumsum(C[::-1])[::-1]) / 2 * dth
 
 
 # Plot distributions
 def plot_distributions(H, K):
-    plt.polar(theta, psi_star)
+    plt.polar(theta, p_th)
     plt.polar(theta, compute_psi(theta, H, K))
 
 
-def compute_psi(theta, H, K):
-    psi = np.exp(
-        H / 2 * np.cos(2 * theta) + K * np.einsum("m,mnk,mnkj", p_star, coeffs, cos)
-    )
-    psi /= trapezoid(psi, theta)
+def grad_psi_f():
+    integrand = np.einsum("ijkl,i,j->ijkl", pcf_th, r * F, np.sin(phi)) * rho
+    integral = trapezoid(integrand.sum(axis=1) * dphi, r, axis=0)
+    grad = np.cumsum(integral, axis=0) * dth
+    return grad
+
+
+# TODO fix this !!
+def gradient_descent_psi(H, K):
+    fig, ax = plt.subplots(subplot_kw={"projection": "polar"})
+    psi = np.roll(norm.pdf(np.linspace(-np.pi, np.pi, Nth), scale=np.pi / 4), Nth // 2)
+    lr = 0.1
+    # mu = 10.0
+    tol = 1e-6
+    max_iter = 1000
+    grad_f = grad_psi_f()
+    for i in range(max_iter):
+        ax.cla()
+        psi_star = np.exp(H / 2 * np.cos(2 * theta) + K * compute_f(theta, psi))
+        psi_star /= psi_star.sum() * dth
+        grad_f_avg = (psi_star[:, np.newaxis] * grad_f).sum(axis=0, keepdims=True) * dth
+        grad_L = (
+            (psi - psi_star) / psi
+            - K
+            * ((grad_f - grad_f_avg) * (psi - psi_star)[:, np.newaxis]).sum(axis=0)
+            * dth
+            # + mu * np.sign(psi.sum() * dth - 1)
+        )
+        new_psi = psi - lr * grad_L
+        new_psi /= new_psi.sum() * dth
+        ax.plot(theta, new_psi)
+        fig.show()
+        if i == 0:
+            plt.pause(1)
+        plt.pause(0.01)
+        if np.linalg.norm(psi - new_psi) < tol:
+            print(f"Gradient descent converged in {i} iterations")
+            return psi
+        psi = new_psi
+        # break
     return psi
 
 
-# def compute_gradient(H, K):
-#     psi = compute_psi(theta, H, K)
-#     cos_avg = trapezoid(cos * psi, theta)
-#     cos2_avg = trapezoid(np.cos(2 * theta) * psi, theta)
-#     grad_H = 0.5 * trapezoid((np.cos(2 * theta) - cos2_avg) * psi * (psi - psi_star))
-#     grad_K = trapezoid(
-#         np.einsum("m,mnk,mnkj", p_star, coeffs, cos - cos_avg[..., None])
-#         * psi
-#         * (psi - psi_star)
-#     )
-#     return grad_H, grad_K
+def compute_psi(theta, H, K):
+
+    psi = np.exp(H / 2 * np.cos(2 * theta) + K * f)
+    psi /= psi.sum() * dth
+    return psi
+
+
+def compute_f(theta, psi):
+    integrand = -(
+        np.einsum(
+            "ijkl,i,j,l->ijkl",
+            pcf_th,
+            r * F,
+            np.sin(phi),
+            psi,
+        )
+        * rho
+    )
+    C = trapezoid(integrand.sum(axis=(1, 3)) * dth * dphi, r, axis=0)
+
+    f = (
+        -(
+            cumulative_trapezoid(C, theta, initial=0)
+            + cumulative_trapezoid(C[::-1], theta[::-1], initial=0)[::-1]
+        )
+        / 2
+    )
+    return f
+
+
+def grad_f(theta, psi):
+    return
 
 
 def compute_gradient(H, K):
     psi = compute_psi(theta, H, K)
-    e = np.cos(np.outer(np.arange(n_modes[0]), theta))
-    p = trapezoid(e * psi, theta)
-    cos2_avg = trapezoid(np.cos(2 * theta) * psi, theta)
-    cos_avg = trapezoid(cos * psi, theta)
+    cos2_avg = (np.cos(2 * theta) * psi).sum() * dth
+    f_avg = (f * psi).sum() * dth
 
-    grad_H = 0.5 * np.dot(
-        trapezoid(e * (np.cos(2 * theta) - cos2_avg) * psi, theta), p - p_star
+    grad_H = (
+        0.5
+        * ((np.cos(2 * theta) - cos2_avg) * (np.log(psi) - np.log(p_th))).sum()
+        * dth
     )
+    grad_K = ((f - f_avg) * (np.log(psi) - np.log(p_th))).sum() * dth
 
-    grad_K = np.dot(
-        trapezoid(
-            np.einsum(
-                "m,mnk,ij,mnkj->ij", p_star, coeffs, e, (cos - cos_avg[..., None])
-            )
-            * psi,
-            theta,
-        ),
-        p - p_star,
-    )
     return grad_H, grad_K
 
 
-H = 3.0  # initial guess
-K = 5
+# H = 1.0  # initial guess
+# K = 5
 
-lr = 0.1
-tol = 1e-6
-max_iter = 10000
+# lr = 0.1
+# tol = 1e-6
+# max_iter = 10000
 
-for i in range(max_iter):
-    grad_H, grad_K = compute_gradient(H, K)
-    new_H = H - lr * grad_H
-    new_K = K - lr * grad_K
-    if abs(new_H - H) < tol and abs(new_K - K) < tol:
-        H = new_H
-        K = new_K
-        print(f"Converged in {i} iterations")
-        break
-    H = new_H
-    K = new_K
+# for i in range(max_iter):
+#     grad_H, grad_K = compute_gradient(H, K)
+#     new_H = H - lr * grad_H
+#     new_K = K - lr * grad_K
+#     if abs(new_H - H) < tol and abs(new_K - K) < tol:
+#         H = new_H
+#         K = new_K
+#         print(f"Converged in {i} iterations")
+#         break
+#     H = new_H
+#     K = new_K
 
-plot_distributions(H, K)
+# print(f"H={H}, K={K}")
+# plot_distributions(H, K)
+
+psi = gradient_descent_psi(3.0, 10.0)
+plt.polar(theta, psi)
