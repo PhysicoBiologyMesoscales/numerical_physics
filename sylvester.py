d2dbd4f0f06a3d94761fd10b892845059529d07d
Nolan Burban <nolanb99@gmail.com>
2025-12-19 14:01:08 +0100

    feat: fixed radius in exponential potential

--- sylvester.py
+++ sylvester.py
@@ -4,53 +4,58 @@ from scipy.linalg import solve_sylvester
 from scipy.special import jv, struve
 from itertools import product
 from tqdm import tqdm
+from matplotlib.colors import CenteredNorm
 
-s = 20
+s = 10
 N = 2 * s + 1
 
 
-def Vexp(k):
-    return 2 * np.pi * np.exp(-0.5 * k**2)
+def Vexp(k, r0=0.5):
+    return 2 * np.pi * r0**2 * np.exp(-0.5 * (r0 * k) ** 2)
 
 
-def Vexp_r(r):
-    return np.exp(-0.5 * r**2)
+def Vexp_r(r, r0=0.5):
+    return np.exp(-0.5 * (r / r0) ** 2)
 
 
-def dVexp_r(r):
-    return -r * np.exp(-0.5 * r**2)
+def dVexp_r(r, r0=0.5):
+    return -r / r0 * np.exp(-0.5 * (r / r0) ** 2)
 
 
 def Vharm(k):
     _v = (
-        2
+        4
         * np.pi
         * (
             1
             / k**2
             * (
-                -2 * jv(2, k)
-                + np.pi * (jv(1, k) * struve(0, k) - jv(0, k) * struve(1, k))
+                -2 * jv(2, 2 * k)
+                + np.pi
+                * (jv(1, 2 * k) * struve(0, 2 * k) - jv(0, 2 * k) * struve(1, 2 * k))
             )
         )
-    ) / 2
-    return np.where(k == 0, 2 * np.pi / 24, _v)
+    )
+    return np.where(k == 0, 16 * np.pi / 12, _v)
+
+
+# def Vharm(k):
+#     return 2 * np.pi * eps**2 * np.where(k == 0, 0.5, jv(1, 2 * k) / (2 * k))
 
 
 def Vharm_r(r):
-    return np.where(r <= 1, 0.5 * (1 - r) ** 2, 0)
+    return np.where(r <= 2, 0.5 * (2 - r) ** 2, 0)
 
 
-lp = 1.0
-phi = 0.5
-eps = 1e-2
+def dVharm_r(r):
+    return np.where(r <= 2, -(2 - r), 0)
 
 
 def L(k, lp, phi, eps, V):
     L = np.diag((np.arange(N) - s) ** 2) / lp - 1j * k / 2 * (
         np.eye(N, k=1) + np.eye(N, k=-1)
     )
-    L[s, s] += 8 * phi / eps * V(k) * k**2
+    L[s, s] += 2 * phi / eps * V(k) * k**2
     return L
 
 
@@ -68,7 +73,7 @@ def compute_g(lp, phi, eps, V, k_arr):
     X = np.zeros((N, N, Npoints), dtype=np.complex128)
 
     for i, k in enumerate(k_arr):
-        X[..., i] = (2 * np.pi) ** 4 * solve_sylvester(
+        X[..., i] = solve_sylvester(
             L(k, lp, phi, eps, V),
             L(k, lp, phi, eps, V).conj().T,
             RHS(k, eps, V),
@@ -96,15 +101,11 @@ def convert_to_r(g0, g1, k_arr, r_arr):
 def compute_P(lp, phi, eps, r_arr, dV, g0, g1):
     P = (
         phi
-        * 4
         / np.pi
         * (
             lp / 2
-            - phi
-            * 2
-            / eps
-            * np.trapz(r_arr**2 * dV(r_arr) * (1 + g0 / (2 * np.pi)), r_arr)
-            + phi * 4 / eps * lp * np.trapz(r_arr * dV(r_arr) * g1, r_arr) / (2 * np.pi)
+            - phi / 2 / eps * np.trapz(r_arr**2 * dV(r_arr) * (1 + g0), r_arr)
+            + phi / eps * lp * np.trapz(r_arr * dV(r_arr) * g1, r_arr)
         )
     )
     return P
@@ -146,15 +147,18 @@ def draw_B(B, r_arr, alpha):
 
 if __name__ == "__main__":
     eps = 1e-2
-    Npoints_k = 100
+    Npoints_k = 200
     Npoints_r = 100
-    k_arr = np.linspace(0, 10, Npoints_k)
-    r_arr = np.linspace(0, 10, Npoints_r)
+    k_arr = np.linspace(0, 20, Npoints_k)
+    r_arr = np.linspace(0, 4, Npoints_r)
     N_lp = 10
     N_phi = 10
-    phi = 0.5
-    lp_arr = np.linspace(10, 200, N_lp)
-    phi_arr = np.linspace(0.05, 1, N_phi)
+    phi_min = 0.0
+    phi_max = 1.0
+    lp_min = 0.01
+    lp_max = 200.0
+    lp_arr = np.linspace(lp_min, lp_max, N_lp)
+    phi_arr = np.linspace(phi_min, phi_max, N_phi)
     list_P = np.zeros((N_phi, N_lp))
     for i, (phi, lp) in enumerate(tqdm(product(phi_arr, lp_arr))):
         _X, g0, g1 = compute_g(lp, phi, eps, Vexp, k_arr)
@@ -162,9 +166,20 @@ if __name__ == "__main__":
         row, col = np.unravel_index(i, (N_phi, N_lp))
         list_P[row, col] = compute_P(lp, phi, eps, r_arr, dVexp_r, g0_r, g1_r)
 
-    ll, pp = np.meshgrid(lp_arr, phi_arr)
-    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-    ax.plot_surface(ll, pp, list_P, linewidth=0, antialiased=False)
+    plt.imshow(
+        list_P.T,
+        extent=(phi_min, phi_max, lp_min, lp_max),
+        aspect="auto",
+        origin="lower",
+        cmap="seismic",
+        norm=CenteredNorm(0),
+    )
+    plt.colorbar()
+    plt.xlabel(r"$\Phi$")
+    plt.ylabel(r"$l_p$")
+# ll, pp = np.meshgrid(lp_arr, phi_arr)
+# fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+# ax.plot_surface(ll, pp, list_P, linewidth=0, antialiased=False)
 
 # plt.plot(lp_arr, list_P.T)
 
