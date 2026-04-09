6cae6d4e1eca11e248dc56cf954bf1483f2e2a05
Nolan Burban <nolanb99@gmail.com>
2026-04-09 17:36:33 +0200

    fix: fixed transitions from light to shadoow insed shadow of other particles

--- 3body_HS.py
+++ 3body_HS.py
@@ -1,5 +1,4 @@
 import numpy as np
-from scipy.interpolate import RegularGridInterpolator
 
 
 class ThreeBodyG:
@@ -280,6 +279,43 @@ class ThreeBodyG:
             tr_vu_plus = changed_vu & (sr_cross[i] > 0)
             tr_vu_minus = changed_vu & (sr_cross[i] < 0)
             transition = tr_v_plus | tr_v_minus | tr_vu_plus | tr_vu_minus
+
+            # Remove transitions where bulk characteristics have already collided with another particles
+            # Check particle 2 collisions for v-tube transitions
+            is_in_shadow_vu = (tr_v_minus) & (in_shadow_vu[i + 1])
+            if np.any(is_in_shadow_vu):
+                t_coll_2 = np.nan_to_num(coll_time[is_in_shadow_vu], 0).astype(int)
+                dt_coll = t[t_coll_2, is_in_shadow_vu] - t[i + 1, is_in_shadow_vu]
+                dt_from_1 = np.dot(s[i + 1, is_in_shadow_vu], self.v) / self.v_norm
+                collided_before = dt_coll < dt_from_1
+                tr_v_minus[is_in_shadow_vu] &= ~collided_before
+
+            # Check particle 1 collisions for v-tube transitions
+            is_in_shadow_vu = (tr_v_plus) & (in_shadow_vu[i + 1])
+            if np.any(is_in_shadow_vu):
+                t_coll_1 = np.nan_to_num(coll_time[is_in_shadow_vu], 0).astype(int)
+                dt_coll = t[t_coll_1, is_in_shadow_vu] - t[i + 1, is_in_shadow_vu]
+                dt_from_1 = np.dot(s[i + 1, is_in_shadow_vu], self.v) / self.v_norm
+                collided_before = dt_coll < dt_from_1
+                tr_v_plus[is_in_shadow_vu] &= ~collided_before
+
+            # Check particle 1 collisions for vu-tube transitions
+            is_in_shadow_v = (tr_vu_plus) & (in_shadow_v[i + 1])
+            if np.any(is_in_shadow_v):
+                t_coll_1 = np.nan_to_num(coll_time[is_in_shadow_v], 0).astype(int)
+                dt_coll = t[t_coll_1, is_in_shadow_v] - t[i + 1, is_in_shadow_v]
+                dt_from_2 = np.dot(sr[i + 1, is_in_shadow_v], self.vu) / self.vu_norm
+                collided_before = dt_coll < dt_from_2
+                tr_vu_plus[is_in_shadow_v] &= ~collided_before
+
+            # Check particle 2 collisions for vu-tube transitions
+            is_in_shadow_v = (tr_vu_minus) & (in_shadow_v[i + 1])
+            if np.any(is_in_shadow_v):
+                t_coll_2 = np.nan_to_num(coll_time[is_in_shadow_v], 0).astype(int)
+                dt_coll = t[t_coll_2, is_in_shadow_v] - t[i + 1, is_in_shadow_v]
+                dt_from_2 = np.dot(sr[i + 1, is_in_shadow_v], self.vu) / self.vu_norm
+                collided_before = dt_coll < dt_from_2
+                tr_vu_minus[is_in_shadow_v] &= ~collided_before
 
             # When crossing, jump is set by the value of outgoing contact distribution
             # Compute the jump value by finding the corresponding point on the 1-3 or 2-3 hyperplane
@@ -563,7 +599,7 @@ if __name__ == "__main__":
     import matplotlib.pyplot as plt
 
     u = np.array([np.cos(-np.pi), np.sin(-np.pi)])
-    v = np.array([np.cos(np.pi / 2), np.sin(np.pi / 2)])
+    v = np.array([np.cos(np.pi / 3), np.sin(np.pi / 3)])
     solver = ThreeBodyG(u, v, plane=1)
     npoints = 50
     _coord = np.linspace(-1.5, 6, npoints)
